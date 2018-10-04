ea90dd030903e033db9df0f4065011ae62abc826
ACJMR <chosking@stuy.edu>
2018-10-03 20:10:40 -0400

    finished work

--- makefile
+++ makefile
@@ -1,10 +1,11 @@
 all: structTest.o
 	gcc structTest.o
 
-stryctTest.o: structTest.c
+structTest.o: structTest.c
 	gcc -c structTest.c
 
 run:
 	./a.out
+    
 clean:
 	rm *.o *.out
--- structTest.c
+++ structTest.c
@@ -1,3 +1,9 @@
+
+//Colin Hosking
+//Work 06
+//Systems pd05
+//2018-10-04
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
@@ -6,33 +12,50 @@
 struct restaurant {char name[200]; int rating;}; 
 
 struct restaurant create(){
-  srand(time(NULL));
   struct restaurant ferrys;
 
   char s[] = "Ferry's";
-  strcpy(ferrys.name,s);
-  ferrys.rating = rand();
+  strcpy(ferrys.name,s);           //copies string literal into name
+  ferrys.rating = rand() % 101;    //takes random into and makes it an int between 0 and 100
 
   return ferrys;
 }
 
 int printRestaurant(struct restaurant r){
-  printf("%s has a rating of %d\n",r.name,r.rating % 101);
+  printf("%s has a rating of %d\n",r.name,r.rating); 
   return 0;
 }
 
 int modify(char newName[200], int newRating, struct restaurant *p){
-  strcpy(p->name,newName);
-  p->rating = newRating;
+  strcpy(p->name,newName);      //copies string literal into name using pointer
+  p->rating = newRating;        
   return 0;
 }
 
 int main(){
+  srand(time(NULL));
+  printf("Creating three restaurants:\n");
+      
+  struct restaurant s = create();
   struct restaurant r = create();
-  struct restaurant *p = &r;
+  struct restaurant t = create();
+  printRestaurant(s);  
   printRestaurant(r);
-  char s[] = "Shake Shack";
-  modify(s, 40, p);
+  printRestaurant(t);
+    
+  printf("\nChange name and rating of first restaurant to 'Jimmy's' and 96\n");
+  struct restaurant *p = &s;
+  char s1[] = "Jimmy's";
+  modify(s1, 96, p);
+  printRestaurant(s);
   printRestaurant(r);
+  printRestaurant(t);
   
+  printf("\nChange name and rating of second restaurant to 'Shake Shack' and 47\n");
+  struct restaurant *q = &r;
+  char s2[] = "Shake Shack";
+  modify(s2, 47, q);
+  printRestaurant(s);
+  printRestaurant(r);
+  printRestaurant(t);
 }
